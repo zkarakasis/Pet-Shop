0e068fdb6fa99c6973d9238b0b45b345588a8249
zkarakasis <zkarakasis@york.citycollege.eu>
2025-11-27 16:57:06 +0200

    Initial project upload

---
+++ Sample Website Map.vb
@@ -0,0 +1,72 @@
+Main page{
+    Header{
+        Pet shop logo and name
+        List with navigation links{
+            Home
+            Animals
+            Products/Accessories
+            About Us/ Contact
+            Interactive Quiz
+        }
+        Fake Shoping Cart
+    }           
+    Main body{
+        A banner with images of some of the pets
+        New products/ arrivals
+        Small introduction about us
+        Customer reviews
+        Fake Newsletter Signup
+    }    
+    Footer{
+        Copyright text
+        Fake social media pages
+        Contact info
+    }
+}        
+Main product pages{
+        Header{
+            Same as main page
+        }    
+        Main body{
+            Price range slider that works as a filter
+            Pet cards{
+                Image
+                Name
+                Price
+                Add to cart button
+            }    
+        }        
+        Footer{
+            Same as main page
+        }    
+}
+Item description Pages{
+    Header{
+        Same as main page
+    }    
+    Main body{
+        Larger image of the pet the user clicked
+        Full description
+        Availability
+        Price
+        Add to cart button
+    }    
+    Footer{
+        Same as main page
+    }    
+}        
+Interactive Quiz Page{
+    Header{
+        Same as main page
+    }    
+    Main body{
+        A few simple multiple choice questions which help the user figure out what pet is best for them
+    }    
+    Footer{
+        Same as main page
+    }    
+}
+    
+    
+
+        
